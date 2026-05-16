d34c48ceff8ff666757992d2bdda2aea6ff9dee2
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-05-17 02:12:22 +0530

    Sync leetcode submission: count-and-say

---
+++ solutions/count-and-say.java
@@ -0,0 +1,1 @@
+private String next(String s) {StringBuilder sb = new StringBuilder();int count = 1;for (int i = 1; i < s.length(); i++) {if (s.charAt(i) == s.charAt(i - 1)) {count++;} else {sb.append(count).append(s.charAt(i -1));count = 1;}}sb.append(count).append(s.charAt(s.length() -1));return sb.toString();}}
