c463a6b5d57f0d073e7ba08b797e4ed485050409
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-05-20 20:47:12 +0530

    Sync leetcode submission: search-a-2d-matrix

--- solutions/search-a-2d-matrix.java
+++ solutions/search-a-2d-matrix.java
@@ -1,1 +1,1 @@
-class Solution {public boolean searchMatrix(int[][] matrix, int target) {int rows = matrix.length;int col = cols - 1;while(row <= col){if(matrix[row][col] == target) return true;if(matrix[row][col] > target) col--;else row++;}return false;}int row = 0;int cols = matrix[0].length;}
+class Solution {public boolean searchMatrix(int[][] matrix, int target) {int rows = matrix.length;int col = cols - 1;while(row < rows && col >= 0){if(matrix[row][col] == target) return true;if(matrix[row][col] > target) col--;else row++;}return false;}int row = 0;int cols = matrix[0].length;}
