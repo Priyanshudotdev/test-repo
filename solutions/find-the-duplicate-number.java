0320f41371f651c7176a46607f5acbf6597a2e6a
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-05-21 22:57:31 +0530

    Sync leetcode submission: find-the-duplicate-number

---
+++ solutions/find-the-duplicate-number.java
@@ -0,0 +1,1 @@
+public static void main(String[] args) {int[] nums = {1, 3, 4, 2, 2};System.out.println(findDuplicate(nums));}public static int findDuplicate(int[] nums) {Set<Integer> seen = new HashSet<>();for (int n : nums) {if (seen.contains(n)) {return n;}seen.add(n);}return -1;}}
