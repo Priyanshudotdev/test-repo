ead016f0594614c771f7ad76e349795bde01c311
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-05-26 01:15:58 +0530

    Sync leetcode submission: move-zeroes

--- solutions/move-zeroes.java
+++ solutions/move-zeroes.java
@@ -1,1 +1,1 @@
-class Solution {public void moveZeroes(int[] nums) {int i = 0;int j = 1;while(i < j && j < nums.length){if(nums[i] == 0 && nums[j] != 0){int temp = nums[i];}nums[i] = nums[j];nums[j] = temp;// increment but waitelse if(nums[j] == 0 && nums[i] == 0){i--;}
+class Solution {public void moveZeroes(int[] nums) {int i = 0;int j = 1;while(i < j && j < nums.length){if(nums[i] == 0 && nums[j] != 0){int temp = nums[i];}nums[i] = nums[j];nums[j] = temp;else if(nums[j] == 0 && nums[i] == 0){i--;}i++;
