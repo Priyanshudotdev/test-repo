d934e6df12f6c9bfc97f7d2fd12fe42e8b497706
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-05-25 20:05:52 +0530

    Sync leetcode submission: count-subarrays-of-length-three-with-a-condition

---
+++ solutions/count-subarrays-of-length-three-with-a-condition.java
@@ -0,0 +1,1 @@
+class Solution {public int countSubarrays(int[] nums) {int counter = 0;for(int i = 0; i < nums.length; i++){if(i+2 < nums.length && nums[i] + nums[i+2] == nums[i+1]/2){counter++;}}}return counter;}
