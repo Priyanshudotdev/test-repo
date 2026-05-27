0bb6d8045b1bc63314c4204f4db332564b3b77d4
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-05-27 11:14:57 +0530

    Sync leetcode submission: contains-duplicate

--- solutions/contains-duplicate.java
+++ solutions/contains-duplicate.java
@@ -1,1 +1,1 @@
-for (int num:nums) {if (!set.add(num)) return true;static boolean betterMethod(int[] nums){HashSet<Integer> set = new HashSet<>();}public boolean containsDuplicate(int[] nums) {return betterMethod(nums);class Solution {}return false;}static boolean bruteForceMethod(int[] nums){Arrays.sort(nums);
+for (int num:nums) {if (!set.add(num)) return true;static boolean betterMethod(int[] nums){Set<Integer> set = new HashSet<>();}public boolean containsDuplicate(int[] nums) {return betterMethod(nums);class Solution {}return false;}static boolean bruteForceMethod(int[] nums){
