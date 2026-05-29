9ccfda2e16c976bf9f4b3f9d39b8a05a09cce261
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-05-30 00:31:53 +0530

    Sync leetcode submission: container-with-most-water

---
+++ solutions/container-with-most-water.go
@@ -0,0 +1,1 @@
+func maxArea(height []int) int {i, j := 0, len(height)-1maxA := 0for i < j {h := height[i]if height[j] < h {h = height[j]}area := h * (j - i)if area > maxA {maxA = area}if height[i] < height[j] {i++} else {j--}}return maxA}
