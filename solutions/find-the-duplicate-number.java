c096e440e70f7ccb9d08ea7a6898adb0d218423b
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-05-30 22:23:10 +0530

    Sync leetcode submission: find-the-duplicate-number

--- solutions/find-the-duplicate-number.java
+++ solutions/find-the-duplicate-number.java
@@ -1,21 +1,21 @@
+int high = nums.length - 1; 
  
+while(low < high){ 
+int mid = low + (high - low) / 2; 
+ 
+int counter = 0; 
+for(int num: nums) if(num <= mid) counter++; 
+ 
+// means there more number trying to fit in it that means there is duplicate numbers 
+if(counter > mid){ 
+high = mid; 
+}else{ 
+low = mid + 1; 
 } 
-return binarySearchMethod(nums); 
-// int[] nums = {1, 3, 4, 2, 2}; 
-public int findDuplicate(int[] nums) { 
+} 
  
-class Solution { 
-import java.util.Set; 
+return low; 
+} 
  
-/* 
-- we are using binary search on the range of number ( 1 - n ) not on the values of array (which 
-*/ 
-is unsorted) 
-- [1,3,4,2,2] ; low = 0; high = n; mid = 4(2) 
-- so will check what is the count of numbers which are lower or equal to mid 
-- because if we check there are 3 number which are less or equals to mid (2) that means 3 
-numbers are trying to fit in [0,2] range bracket that gives hint that there may exisits a 
-duplicate number in left 
-- so we just make high = mid; 
-- else its lesser means check at right side : low = mid + 1; 
-- at the end return low 
+static int hashMapMethod(int[] nums){ 
+Map<Integer, Integer> seen = new HashMap<>(); 
