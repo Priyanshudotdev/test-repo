caec4b9541960734b694da5ccb8c574477427a09
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-05-30 21:52:52 +0530

    Sync leetcode submission: contains-duplicate

--- solutions/contains-duplicate.java
+++ solutions/contains-duplicate.java
@@ -1,20 +1,21 @@
- 
-// sort arr 
-} 
- 
-class Solution { 
-public boolean containsDuplicate(int[] nums) { 
-import java.util.HashSet; 
-import java.util.Set; 
-// static boolean betterMethod(int[] nums){ 
-//     Set<Integer> set = new HashSet<>(); 
+// return false; 
+// } 
+//     i++; 
+//     } 
+//         return true; 
+//     if(nums[i] == nums[i+1]){ 
+// while(i+1 < nums.length){ 
+// int i = 0; 
+// Arrays.sort(nums); 
 // check if arr[i] == arr[i+1] 
-Arrays.sort(nums); 
-int i = 0; 
-while(i+1 < nums.length){ 
-if(nums[i] == nums[i+1]){ 
-return true; 
+// sort arr 
+ 
+Map<Integer, Integer> seen = new HashMap<>(); 
+ 
+for(int num: nums){ 
+if(seen.containsKey(num)) return true; 
+seen.put(num,1); 
 } 
-i++; 
-} 
+ 
 return false; 
+ 
