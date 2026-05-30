cb23eeccb942c8e2accc5de809acb8c676a2b118
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-05-30 22:47:41 +0530

    Sync leetcode submission: move-zeroes

--- solutions/move-zeroes.java
+++ solutions/move-zeroes.java
@@ -1,1 +1,20 @@
-class Solution {public void moveZeroes(int[] nums) {int i = 0;int j = 1;while(i < j && j < nums.length){if(nums[i] == 0 && nums[j] != 0){int temp = nums[i];}nums[i] = nums[j];nums[j] = temp;else if(nums[j] == 0 && nums[i] == 0){i--;}i++;
+*/ 
+- increment i and j 
+[-1 + 1 = 0]) 
+ 
+int i = 0; 
+int j = 1; 
+ 
+while(i < nums.length && j < nums.length){ 
+if(nums[i] == 0 && nums[j] != 0){ 
+- case - 2 : if n[i] == n[j] == 0: 
+- decremeant i (because we are increamenting i outside the if-else statement to make sure 
+that i stays at its place we are decreamenting inside the loop and incrementing outside 
+- swap elements 
+- j -> will track non-zeros 
+- case - 1 : if n[i] == 0 n[j] != 0: 
+- i -> will track zeros 
+- will take two pointer 
+/* 
+public void moveZeroes(int[] nums) { 
+class Solution { 
