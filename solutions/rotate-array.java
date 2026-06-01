fb771790b54b92363541dc946bfcb1f7d674dc15
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-01 22:31:03 +0530

    Sync leetcode submission: rotate-array

--- solutions/rotate-array.java
+++ solutions/rotate-array.java
@@ -1,20 +1,20 @@
-int temp = nums[start]; 
 nums[start] = nums[end]; 
-nums[end] = temp; 
+int temp = nums[start]; 
+ 
 while(start <= end){ 
 static void reverseArr(int[] nums, int start, int end){ 
-} 
- 
+if(end > nums.length - 1) return; 
 reverseArr(nums, 0, k-1); 
 reverseArr(nums, k, nums.length - 1); 
+} 
+ 
+static void optimalMethod(int[] nums, int k){ 
+// reverse the entire arr 
 reverseArr(nums, 0, nums.length - 1); 
 // now reverse 0 - k and k + 1 to n(length of arr) 
-// reverse the entire arr 
-static void optimalMethod(int[] nums, int k){ 
- 
 optimalMethod(nums,k); 
 } 
-public void rotate(int[] nums, int k) { 
-class Solution { 
-if(end > nums.length - 1) return; 
  
+class Solution { 
+public void rotate(int[] nums, int k) { 
+k = k % nums.length; 
