897c61e1ca09de7d60ca350ee37e8ef99cf498ef
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-03 01:47:04 +0530

    Sync leetcode submission: the-kth-factor-of-n

---
+++ solutions/the-kth-factor-of-n.java
@@ -0,0 +1,16 @@
+class Solution { 
+public int kthFactor(int n, int k) { 
+return bruteForceMethod(n,k); 
+} 
+ 
+static int bruteForceMethod(int n, int k){ 
+List<Integer> list = new ArrayList<>(); 
+if(n % i == 0) list.add(i); 
+} 
+for(int i = 1; i <= n; i++){ 
+ 
+ 
+return result; 
+int result = k-1 < list.size() ? list.get(k-1) : -1; 
+} 
+} 
