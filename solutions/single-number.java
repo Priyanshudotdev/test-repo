a58eefc01cf88630ef53e5785513c433404f5e34
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-03 01:27:37 +0530

    Sync leetcode submission: single-number

--- solutions/single-number.java
+++ solutions/single-number.java
@@ -1,22 +1,23 @@
-import java.util.Map; 
-import java.util.HashMap; 
-import java.util.Objects; 
- 
 class Solution { 
 public int singleNumber(int[] nums) { 
+return optimalMethod(nums); 
+} 
+ 
+static int bruteForcecMethod(int[] nums){ 
 Map<Integer, Integer> map = new HashMap<>(); 
  
 for(int num: nums){ 
+import java.util.Objects; 
+ 
+ 
+static int optimalMethod(int[] nums){ 
+int res = 0; 
+} 
+for(int num: nums){ 
+res = num ^ res; 
+} 
+ 
+return res; 
 map.put(num, map.getOrDefault(num,0) + 1 ); 
 } 
  
-for(Map.Entry<Integer,Integer> entry: map.entrySet()){ 
-value = entry.getValue(); 
-} 
-int value = 0; 
-if(value == 1){ 
-return entry.getKey(); 
-} 
-} 
-return -1; 
-} 
