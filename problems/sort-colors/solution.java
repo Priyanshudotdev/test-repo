8cbc281778115ce444235a2384e2473c79df88d2
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-11 22:53:47 +0530

    Sync leetcode submission: sort-colors

---
+++ problems/sort-colors/solution.java
@@ -0,0 +1,18 @@
+        int blue = 0; // 2
+        for(int num:nums){
+            if(num == 0) red++;
+            else if(num == 1) white++;
+            else blue++;
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(red > 0){
+                nums[i] = 0;
+                red--;
+        int white = 0; // 1
+        int red = 0; // 0
+            - loop through the array and replace colors according to number of color
+        */
+            - find the number of color for red, white and blue
+        /*
+     static void bruteForceMethod(int[] nums){
+    }
