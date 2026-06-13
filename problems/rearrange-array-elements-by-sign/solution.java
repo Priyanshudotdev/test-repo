3f4f765d939bd5c4dbfa58272e12e88093bbe5c6
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-13 21:04:32 +0530

    Sync leetcode submission: rearrange-array-elements-by-sign

---
+++ problems/rearrange-array-elements-by-sign/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int[] result = new int[nums.length];
+        
+        int i = 0;
+        int j = 1;
+        
+        for(int k = 0; k < nums.length; k++){
+            if(nums[k] > 0) {
+                result[i] = nums[k];
+                i += 2;
+            }else{
+                result[j] = nums[k];
+                j += 2;
+            }
+        }
+        
+        return result;
+        // List<Integer> list = new ArrayList<>();
+        // int lastNum = 0;
+        // int i = 0;
