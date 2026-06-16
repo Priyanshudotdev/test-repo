70e1552d46a82601bf6a7e416e868a161e4b81e2
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-16 22:01:27 +0530

    Sync leetcode submission: longest-consecutive-sequence

---
+++ problems/longest-consecutive-sequence/solution.java
@@ -0,0 +1,16 @@
+        for(int num: nums){
+            set.add(num);
+        }
+        //step-1
+        int longest = 0;
+        Set<Integer> set = new HashSet<>();
+        }
+        if(nums.length == 0 || nums == null){
+            return 0;
+        // edge case
+        // re-solve
+        
+    public int longestConsecutive(int[] nums) {
+class Solution {
+import java.util.Set;
+import java.util.HashSet;
