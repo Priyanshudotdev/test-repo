a1009e7e9f192537f60ee067f36853d2a417426e
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-16 21:04:06 +0530

    Sync leetcode submission: next-permutation

--- problems/next-permutation/solution.java
+++ problems/next-permutation/solution.java
@@ -1,18 +1,19 @@
+        }
+            return;
         if(breakPoint == 0){
             reverseArr(nums, 0, nums.length - 1);
-            return;
-        }
-        array and will the first permutation (the actual array) 
-        //step-2 : find the elem bigger than the break point elem from right
-        for(int j = nums.length - 1; j >= 0; j--){
-            if(nums[j] > nums[breakPoint]){
-                elemIndex = j;
-                break;
+        // System.out.println("elemIndex " + breakPoint);
             }
-        }
-        //step-3 : now replace the elements
-        swapElem(nums,breakPoint, elemIndex);
-        //step-4 : now reverse the right subarray from breakPoint + 1 to n-1
-        reverseArr(nums,breakPoint+1, nums.length - 1);    
-    }
-    static void swapElem(int[] nums, int i, int j){
+        }  
+        
+            if(nums[i - 1] < nums[i]){
+                breakPoint = i;
+                break;
+        for(int i = nums.length - 1; i > 0; i--){
+        //step-1 : find the break point where (from back) where a[i] < a[i+1]
+        
+        int breakPoint = 0;
+        int elemIndex = 0;
+    // re-re-solve
+class Solution {
+    static void nextPermutation(int[] nums){
