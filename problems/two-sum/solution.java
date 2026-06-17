4c7f6b0c6fb5170fbde0b047e385d53c0acc96ce
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-17 22:02:06 +0530

    Sync leetcode submission: two-sum

--- problems/two-sum/solution.java
+++ problems/two-sum/solution.java
@@ -1,15 +1,19 @@
 import java.util.HashMap;
 import java.util.Map;
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> seen = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
+    public int[] twoSum(int[] nums, int 
+    target) {
+        Map<Integer, Integer> seen = new 
+        HashMap<>();
+        for (int i = 0; i < nums.length; i
+        ++) {
             int need = target - nums[i];
             if (seen.containsKey(need)) {
-                return new int[] { seen.get(need), i };
+                return new int[] { seen.get
+                (need), i };
             }
             seen.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No two sum solution");
+        throw new IllegalArgumentException
+        ("No two sum solution");
     }
-}
