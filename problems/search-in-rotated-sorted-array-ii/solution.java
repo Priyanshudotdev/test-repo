c5c5c155cf649047855924215a9139ab56fdcb03
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-20 22:30:33 +0530

    Sync leetcode submission: search-in-rotated-sorted-array-ii

--- problems/search-in-rotated-sorted-array-ii/solution.java
+++ problems/search-in-rotated-sorted-array-ii/solution.java
@@ -1,17 +1,17 @@
-            int mid = low + (high - low) / 2;
-            if(nums[mid] == target) return true;
-            if(nums[low] == nums[mid]){
-        while(low <= high){
-        if (low > high || nums == null || nums.length == 0) return false;
-    public boolean bSearch(int[] nums, int target, int low, int high){
-        if(bSearch(nums,target,0,i)){
+            return false;
+        }
+        return bSearch(nums, target, i + 1, nums.length - 1);
+    }
+    public static boolean bSearch(int[] nums, int target, int low, int high) {
+        if (low > high || nums == null || nums.length == 0) {
+        }
+        if (bSearch(nums, target, 0, i)) {
             return true;
         }
-        return bSearch(nums,target,i+1, nums.length-1);
-    }
+        int i = 0;
+        while (i + 1 < nums.length && nums[i] <= nums[i + 1]) {
             i++;
-        }
-    public boolean search(int[] nums, int target) {
-        int i = 0; // breaking point
-        while(i+1 < nums.length && nums[i] < nums[i+1]){
 class Solution {
+    public static boolean search(int[] nums, int target) {
+        if (nums[low] == nums[high - 1]) {
+            low = high;
