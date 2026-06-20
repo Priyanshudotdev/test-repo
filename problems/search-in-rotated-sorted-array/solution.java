96349fcaac8a21f103fdbb116451547532b72008
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-20 20:20:37 +0530

    Sync leetcode submission: search-in-rotated-sorted-array

---
+++ problems/search-in-rotated-sorted-array/solution.java
@@ -0,0 +1,17 @@
+        }
+    }
+    public static int bSearchReSolve(int nums[], int target, int low, int high) {
+        while (low <= high) {
+            // return result1;
+            return result1;
+            return bSearchReSolve(nums, target, i + 1, nums.length - 1);
+        } else {
+        int result1 = bSearchReSolve(nums, target, 0, i);
+        if (result1 == -1) {
+            i++;
+        }
+        int i = 0; // break point
+        while (i+1 < nums.length && nums[i] < nums[i + 1]) {
+        // Re-Solve
+    public int search(int[] nums, int target) {
+class Solution {
