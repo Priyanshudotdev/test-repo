031c3b7177d2f020acc2350dd0d2dd1538a7ff4e
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-20 20:39:11 +0530

    Sync leetcode submission: search-in-rotated-sorted-array-ii

--- problems/search-in-rotated-sorted-array-ii/solution.java
+++ problems/search-in-rotated-sorted-array-ii/solution.java
@@ -2,16 +2,16 @@
         if (low > high || nums == null || nums.length == 0) return false;
         while(low <= high){
             int mid = low + (high - low) / 2;
-            return true;
-        }
-        return bSearch(nums,target,i+1, nums.length-1);
-    }
             if(nums[mid] == target) return true;
-            if(nums[mid] > target) high = mid - 1;
-        if(bSearch(nums,target,0,i)){
+            if(nums[low] == nums[mid]){
+    }
+        return bSearch(nums,target,i+1, nums.length-1);
         }
+            return true;
+        if(bSearch(nums,target,0,i)){
         while(i+1 < nums.length && nums[i] < nums[i+1]){
             i++;
-    public boolean search(int[] nums, int target) {
+        }
         int i = 0; // breaking point
+    public boolean search(int[] nums, int target) {
 class Solution {
