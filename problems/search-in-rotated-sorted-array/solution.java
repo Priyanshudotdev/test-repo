7c714b5843c0fa4fdb4fad19c186569c9ad33975
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-20 20:22:31 +0530

    Sync leetcode submission: search-in-rotated-sorted-array

--- problems/search-in-rotated-sorted-array/solution.java
+++ problems/search-in-rotated-sorted-array/solution.java
@@ -1,17 +1,19 @@
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
+        return -1;
     }
-    public static int bSearchReSolve(int nums[], int target, int low, int high) {
-        while (low <= high) {
-            // return result1;
-            return result1;
-            return bSearchReSolve(nums, target, i + 1, nums.length - 1);
-        } else {
-        int result1 = bSearchReSolve(nums, target, 0, i);
-        if (result1 == -1) {
-            i++;
-        }
-        int i = 0; // break point
-        while (i+1 < nums.length && nums[i] < nums[i + 1]) {
-        // Re-Solve
-    public int search(int[] nums, int target) {
-class Solution {
+    
+    
+    
+    
+    
+    
+    
