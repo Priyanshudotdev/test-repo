8cd9be4c5fe10760a343c10a1328ebb5cb2f2d61
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-21 20:58:11 +0530

    Sync leetcode submission: koko-eating-bananas

--- problems/koko-eating-bananas/solution.java
+++ problems/koko-eating-bananas/solution.java
@@ -1,17 +1,14 @@
-            int hours = 0;
-            for (int p : piles) {
-                // round to top (ceiling)
-                // hours += Math.ceil(p / k);
-                hours += (p + k - 1) / k; // ceil(p/k) using integers
-            }
             if (hours <= h) {
                 res = Math.min(res, k);
+            }
+                if (hours > h) break;      // optional early stop
+                hours += (lp + k - 1) / k; // ceil(lp / k) safely in long
+                long lp = p;
                 high = k - 1;
             } else {
                 low = k + 1;
             }
         }
-        return res;
-            int k = low + (high - low) / 2;
-        while (low <= high) {
-        int res = high;
+        return (int) res;
+    }
+}
