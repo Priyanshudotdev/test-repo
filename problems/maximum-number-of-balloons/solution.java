ee1cfa985f2c96ec10386d5481b28f389bd5ca1b
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-22 10:40:28 +0530

    Sync leetcode submission: maximum-number-of-balloons

--- problems/maximum-number-of-balloons/solution.java
+++ problems/maximum-number-of-balloons/solution.java
@@ -1,15 +1,15 @@
-                int result = map.getOrDefault(c, 0);
-                map.put(c, result + 1);
-            }
-        }
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+class Solution {
+    public static int maxNumberOfBalloons(String text) {
+        return anotherBroteFoce(text);
+    }
+    public static int anotherBroteFoce(String text) {
+    }
+    public static int broteForce(String text){
+        Map<Character, Integer> map = new HashMap<>();
         // char[] balloonArr = {'b', 'a', 'l', 'l', 'o', 'o', 'n'};
         ArrayList<Character> charArr = new ArrayList<>(List.of('b', 'a', 'l', 'o', 'n'));
         int ballonCount = 0;
-        for (char c : text.toCharArray()) {
-            if (findInArray(charArr, c)) {
-        Map<Character, Integer> map = new HashMap<>();
-class Solution {
-    public static int maxNumberOfBalloons(String text) {
-        if (map.size() != 5) {
-            return ballonCount;
-        }
