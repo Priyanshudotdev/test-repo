30ae3aa52f67f35789c1b2f625935c3f587a14a3
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-25 23:40:36 +0530

    Sync leetcode submission: remove-duplicates-from-sorted-array

--- problems/remove-duplicates-from-sorted-array/solution.java
+++ problems/remove-duplicates-from-sorted-array/solution.java
@@ -1,5 +1,13 @@
-                - j represents the index which which is unique number and will replace 
-                with i index
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        /*
+            - init two pointers
+            - will store the current index (i)
+            - loop through j and check if j's and i-1's num are equal or not
+            - if not equal then replace if not then move forward 
+            (for context) :
+                - i represents the index which whe can replace
+                - j represents the index which which is unique number and will replace with i index
         */ 
         int i = 1;
         int j = 1;
@@ -13,6 +21,3 @@
         }
         return i;
     }
-                - i represents the index which whe can replace
-            (for context) :
-            - if not equal then replace if not then move forward 
