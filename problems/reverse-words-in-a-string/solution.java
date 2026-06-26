ada376e738cc51d2387116ac72324e15dfa36db7
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-26 22:56:56 +0530

    Sync leetcode submission: reverse-words-in-a-string

---
+++ problems/reverse-words-in-a-string/solution.java
@@ -0,0 +1,9 @@
+    }
+        return result;
+        String result = String.join(" ", strArr);
+        reverse(strArr);
+        // System.out.println(Arrays.toString(strArr));
+        String[] strArr = s.trim().split("\\s+");
+    public static String reverseWords(String s) {
+class Solution {
+import java.util.Arrays;
