d3145f86a79bef70a021584ea1df70980acaa7b2
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-06-27 18:01:09 +0530

    Sync leetcode submission: longest-common-prefix

--- problems/longest-common-prefix/solution.java
+++ problems/longest-common-prefix/solution.java
@@ -1,12 +1,13 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        for (int i = 0; i < strs[0].length(); i++) {
-            for (String s : strs) {
-                if (i == s.length() || s.charAt(i) != strs[0].charAt(i)) {
-                    return s.substring(0, i);
-                }
+       String ref = strs[0];
+       for(int i = 0; i < strs.length; i++){
+        for(String s: strs){
+            if(i >= s.length() || s.charAt(i) != ref.charAt(i) ){
+                return ref.substring(0,i);
             }
         }
-        return strs[0];
+       }
+       return "";
     }
 }
