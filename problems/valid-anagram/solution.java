69c4692f567b4c5b507af8eae40f252616e07366
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-02 23:55:14 +0530

    Sync leetcode submission: valid-anagram

--- problems/valid-anagram/solution.java
+++ problems/valid-anagram/solution.java
@@ -1,20 +1,17 @@
-        Map<Character, Integer> map = new HashMap<>();
+        }
+            arr[t.charAt(i) - 'a']--;
+            arr[s.charAt(i) - 'a']++;
         for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-        }
-        for (int i = 0; i < t.length(); i++) {
-            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
-        }
-        for (Map.Entry<Character, Integer> m : map.entrySet()) {
-            int val = m.getValue();
-            if (val % 2 != 0) {
-                return false;
-            return true;
-        }
-        if (s.equals(t)) {
+        int[] arr = new int[50];
         }
             return false;
         if (s.length() != t.length()) {
         }
             return false;
         if (s == null || t == null) {
+    static boolean reSolveOptimal(String s , String t){
+    }
+    public boolean isAnagram(String s, String t) {
+        return reSolveOptimal(s,t);
+class Solution {
+import java.util.Arrays;
