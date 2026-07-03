e5c7b28c5666bce5149066ea44c3b350aa67e963
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-03 23:25:02 +0530

    Sync leetcode submission: reverse-words-in-a-string

--- problems/reverse-words-in-a-string/solution.java
+++ problems/reverse-words-in-a-string/solution.java
@@ -1,9 +1,15 @@
     }
+    public static void reverse(String[] strArr) {
+        int start = 0;
+        int end = strArr.length - 1;
+        while (start < end) {
+            String temp = strArr[start];
+            strArr[start] = strArr[end];
         return result;
         String result = String.join(" ", strArr);
-        reverse(strArr);
-        // System.out.println(Arrays.toString(strArr));
         String[] strArr = s.trim().split("\\s+");
+        // System.out.println(Arrays.toString(strArr));
+        reverse(strArr);
     public static String reverseWords(String s) {
 class Solution {
 import java.util.Arrays;
