eb908f021679b2e763262d6ff456cede53dd61dc
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-03 22:44:08 +0530

    Sync leetcode submission: sort-characters-by-frequency

--- problems/sort-characters-by-frequency/solution.java
+++ problems/sort-characters-by-frequency/solution.java
@@ -1,15 +1,15 @@
-        for (int i = 0; i < s.length(); i++) {
-        }
-        int refVal = -1;
+        // for (int i = 0; i < map.size(); i++) {
+        //     int val = map.get(s.charAt(i));
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
         for (int i = 0; i < s.length(); i++) {
-        StringBuilder result = new StringBuilder();
-        Map<Character, Integer> map = new HashMap<>();
     public static String frequencySort(String s) {
-    // }
-    // public static void main(String[] args) {
-    //     String s = "bananas";
-    //     System.out.println(frequencySort(s));
-class Solution {
-import java.util.Map;
+        Map<Character, Integer> map = new HashMap<>();
+        StringBuilder result = new StringBuilder();
 import java.util.HashMap;
+import java.util.Map;
+class Solution {
+    // public static void main(String[] args) {
+    //     String s = "raaeaedere";
+    //     System.out.println(frequencySort(s));
+    // }
