bfb667aea9c89d04e7ba3b5b91df35826b1d0283
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-05 17:33:22 +0530

    Sync leetcode submission: roman-to-integer

--- problems/roman-to-integer/solution.java
+++ problems/roman-to-integer/solution.java
@@ -1,18 +1,18 @@
-            int nextValue = map.getOrDefault(charArr[i + 1], -1);
-            if (value != -1 && nextValue != -1 && nextValue > value) {
-                result += (nextValue - value);
-                i += 2;
-                continue;
+        map.put("D", 500);
+        map.put("C", 100);
+        map.put("L", 50);
+        map.put("V", 5);
+        map.put("X", 10);
+        map.put("M", 1000);
+        String[] charArr = roman.split("");
+        int i = 0;
+        while (i < charArr.length) {
+            int value = map.getOrDefault(charArr[i], -1);
+            if (i + 1 < charArr.length) {
+                int nextValue = map.getOrDefault(charArr[i + 1], -1);
+                if (value != -1 && nextValue != -1 && nextValue > value) {
+                    result += (nextValue - value);
+                    i += 2;
+                    continue;
+                }
             }
-        }
-        if (i + 1 < charArr.length) {
-    while (i < charArr.length) {
-        int value = map.getOrDefault(charArr[i], -1);
-    int i = 0;
-    String[] charArr = roman.split("");
-    map.put('M', 1000);
-    map.put('D', 500);
-    map.put('C', 100);
-    map.put('L', 50);
-    map.put('X', 10);
-    map.put('V', 5);
