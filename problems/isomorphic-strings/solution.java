3b87459490cc4349fa7909fd0cef6c332351e018
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-05 15:07:45 +0530

    Sync leetcode submission: isomorphic-strings

--- problems/isomorphic-strings/solution.java
+++ problems/isomorphic-strings/solution.java
@@ -1,13 +1,15 @@
-        // }
-        //     map1.put(s.charAt(i), map1.getOrDefault(s.charAt(i), 0) + 1);
-        // for (int i = 0; i < s.length(); i++) {
-        // Map<Character, Integer> map2 = new HashMap<>();
-        // Map<Character, Integer> map1 = new HashMap<>();
-        // }
-        // if (s.length() != t.length()) {
-        //     return false;
-        return reSolve(s,t);
-    public static boolean isIsomorphic(String s, String t) {
-class Solution {
-import java.util.Map;
-import java.util.HashMap;
+    public static boolean optimalMethod(String s, String t){
+    }
+        return true;
+        }
+            m2[t.charAt(i)] = i + 1;
+            m1[s.charAt(i)] = i + 1;
+            }
+                return false;
+            if (m1[s.charAt(i)] != m2[t.charAt(i)]) {
+        int[] m1 = new int[256], m2 = new int[256];
+        for(int i = 0; i < s.length(); i++){
+            if(m1[s.charAt(i)] != m2[t.charAt(i)]) return false;
+            m1[s.charAt(i)] = i + 1;
+        for (int i = 0; i < s.length(); i++) {
+        int[] m2 = new int[256];
