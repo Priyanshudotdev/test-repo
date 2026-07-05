bb1e4bff019340194ac5e5f88665f8762febd1db
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-05 16:41:48 +0530

    Sync leetcode submission: maximum-nesting-depth-of-the-parentheses

--- problems/maximum-nesting-depth-of-the-parentheses/solution.java
+++ problems/maximum-nesting-depth-of-the-parentheses/solution.java
@@ -1,18 +1,16 @@
-    public static int maxDepth(String s) {
-        int maxDepth = 0;
-        int currDepth = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(') {
-                currDepth++;
-                maxDepth = Math.max(currDepth, maxDepth);
-            }
-            if (c == ')') {
-                currDepth--;
-            }
-    // public static void main(String[] args) {
-    //     String s = "(1)+((2))+(((3)))";
-    //     System.out.println(maxDepth(s));
-    // }
-class Solution {
+            count--;
         }
+        if (s.charAt(i) == ')') {
+        maxCount = Math.max(count, maxCount);
+        }
+        if (s.charAt(i) == '(') {
+            count++;
+    for (int i = 0; i < s.length(); i++) {
+    int count = 0;
+    }
+    return maxCount;
+    }
+    public static int maxDepth(String s) {
+        // re-solve 
+        return maxDepthParantheses(s);
+    }
