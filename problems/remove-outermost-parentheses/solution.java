fbc59c84a13ebc2cdb0fdb9698cf529e2a60060c
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-05 13:19:05 +0530

    Sync leetcode submission: remove-outermost-parentheses

--- problems/remove-outermost-parentheses/solution.java
+++ problems/remove-outermost-parentheses/solution.java
@@ -1,18 +1,18 @@
-            if (s.charAt(i) == '(') {
-        for (int i = 0; i < s.length(); i++) {
-        int init = 0;
-        int count = 0;
-        List<StringBuilder> list = new ArrayList<>();
-        StringBuilder result = new StringBuilder();
-    public static String removeParan(String s) {
-    // re-solve
     }
-        return removeParan(s);
-    public String removeOuterParentheses(String s) {
-     */
-        - if the counter is not equals to zero then push to result
-        and dereament if closing ")"
-        - loop to whole string and increament counter if its opening paranthesis "(" 
-        - will take a counter var and a result string
-    /**
-class Solution {
+    // re-solve
+        public static String optimalMethod(String s) {
+        StringBuilder result = new StringBuilder();
+        int counter = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                counter--;
+            }
+            if (counter != 0) {
+                result.append(s.charAt(i));
+            }
+            if (s.charAt(i) == '(') {
+                counter++;
+            }
+        }
+        return result.toString();
+    }
