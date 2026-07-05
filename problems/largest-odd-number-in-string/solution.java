fcacbff88d8ab2ecd5a055050202f822e28eee8e
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-05 14:05:49 +0530

    Sync leetcode submission: largest-odd-number-in-string

--- problems/largest-odd-number-in-string/solution.java
+++ problems/largest-odd-number-in-string/solution.java
@@ -1,11 +1,18 @@
-        String[] strNum = num.split("");
-        for (int i = strNum.length - 1; i >= 0; i--) {
-            int currNum = Integer.parseInt(strNum[i]);
-            if (currNum % 2 != 0) {
-    public static String largestOddNumber(String num) {
-class Solution {
+     index to last
+     * odd number index (simple :)
+     *
+     */
+    public static String largestString(String num) {
+        int lastDigit = (int) num.charAt(num.length() - 1);
+        for (int i = num.length() - 1; i >= 0; i--) {
+            int currDigit = Character.getNumericValue(num.charAt
+            if (currDigit % 2 != 0) {
                 return num.substring(0, i + 1);
+        if (lastDigit % 2 != 0) {
+            return num;
+        }
             }
         }
         return "";
     }
+            (i));
