307634113ffe172bfbe3b37908696fba19aae944
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-07 09:08:08 +0530

    Sync leetcode submission: longest-substring-without-repeating-characters

--- problems/longest-substring-without-repeating-characters/solution.java
+++ problems/longest-substring-without-repeating-characters/solution.java
@@ -2,16 +2,14 @@
         will remove all elements til we get out first "a" 
     public static int optimalMethod(String s){
         Set<Character> set = new HashSet<>();
-        int result = 0;
-        for (int i = 0; i < s.length(); i++) {
-            while (set.contains(s.charAt(i))) {
-        int l = 0;
-                set.remove(s.charAt(l));
-                l += 1;
-            }
-        }
-            set.add(s.charAt(i));
-            result = Math.max(result, i - l + 1);
-        return result;
+        - why ? : let say : s = "abcabcbb", here we reach till c and our substring looks like "abc" so 
+        substring
+        *Optmizid Algorithm*
+        - will check each string and if we got any duplicate string will from the element from that 
+    public static int lengthOfLongestSubstring(String s) {
+        return optimalMethod(s);
     }
-    public static int bruteForceMethod(String s){
+    /**
+class Solution {
+import java.util.HashSet;
+import java.util.Set;
