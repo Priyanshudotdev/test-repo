6730db0a9b74dfa6a85a76306ccad4c5cd062242
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-14 19:20:24 +0530

    Sync leetcode submission: sum-of-beauty-of-all-substrings

---
+++ problems/sum-of-beauty-of-all-substrings/solution.java
@@ -0,0 +1,17 @@
+            map.clear();
+            result += count;
+            }
+                    count += sub;
+                }
+                int sub = getFreqSub(map);
+                if(sub > 0){
+                map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1);
+            int count = 0;
+            for(int j = i; j < s.length(); j++){
+        for(int i = 0; i < s.length(); i++){
+         Map<Character, Integer> map = new HashMap<>();
+         int result = 0;
+     public static int beautySum(String s) {
+class Solution {
+import java.util.Map;
+import java.util.HashMap;
