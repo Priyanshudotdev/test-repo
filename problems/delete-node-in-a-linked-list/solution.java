c0830b9fbb0a7d01fbab64ee4a658ec5bb248520
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-15 20:41:43 +0530

    Sync leetcode submission: delete-node-in-a-linked-list

---
+++ problems/delete-node-in-a-linked-list/solution.java
@@ -0,0 +1,12 @@
+//  Definition for singly-linked list.
+ public class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+  }
+class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode nextOne = head.next;
+        System.out.println(nextOne);
+    }
+}
