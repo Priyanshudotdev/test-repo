483e72ffd5f8ed63d4e7799d0c93a92f1f8d0fc2
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-15 22:46:57 +0530

    Sync leetcode submission: middle-of-the-linked-list

---
+++ problems/middle-of-the-linked-list/solution.java
@@ -0,0 +1,20 @@
+            temp = temp.next;
+            i++;
+        while(temp != null){
+        int i = 0;
+        }
+        return get(i,head);
+        ListNode temp = head;
+    public ListNode len(ListNode head){
+    }
+        return len(head);   
+    public ListNode middleNode(ListNode head) {
+class Solution {
+ */
+ * }
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     ListNode next;
+ *     ListNode() {}
+ *     int val;
+    }
