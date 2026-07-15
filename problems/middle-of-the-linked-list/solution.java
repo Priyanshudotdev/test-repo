7d8ece0ffb3627eafd8e9732b5ce4d6e30f6e73f
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-15 22:53:32 +0530

    Sync leetcode submission: middle-of-the-linked-list

--- problems/middle-of-the-linked-list/solution.java
+++ problems/middle-of-the-linked-list/solution.java
@@ -1,20 +1,17 @@
-            temp = temp.next;
-            i++;
-        while(temp != null){
-        int i = 0;
+    /*
+        Optimal Approach (Tortise Method)
+     */
+        - take two node var
+        - one slow and one fast
+        - slow node will take 1 step at a time
+        - fast will take 2 step at a time
+        - when fast node reaches end or null "slow" will be at middle of the linked list
+    public ListNode optimalMethod(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null){
+    }
+            slow = slow.next;
         }
-        return get(i,head);
-        ListNode temp = head;
-    public ListNode len(ListNode head){
-    }
-        return len(head);   
-    public ListNode middleNode(ListNode head) {
-class Solution {
- */
- * }
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- *     ListNode next;
- *     ListNode() {}
- *     int val;
-    }
+            fast = fast.next.next;
+        return slow;
