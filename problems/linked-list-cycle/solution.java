ccc9fa18e2150cc571060550a1d2441f2d19ac4a
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-16 19:47:49 +0530

    Sync leetcode submission: linked-list-cycle

---
+++ problems/linked-list-cycle/solution.java
@@ -0,0 +1,18 @@
+    public boolean bruteForceMethod(ListNode head){
+        ListNode temp = head;
+        HashMap<ListNode, Integer> nodeMap = new HashMap<>();
+    }
+        return bruteForceMethod(head);
+    public boolean hasCycle(ListNode head) {
+public class Solution {
+ */
+ * }
+ *     }
+ *         next = null;
+ *         val = x;
+ *     ListNode(int x) {
+ *     ListNode next;
+ *     int val;
+ * class ListNode {
+ * Definition for singly-linked list.
+/**
