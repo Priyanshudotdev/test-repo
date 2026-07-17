8c7a4f1f64acbb090d43a1dc5bf93ae1e36778c7
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-17 20:04:43 +0530

    Sync leetcode submission: palindrome-linked-list

--- problems/palindrome-linked-list/solution.java
+++ problems/palindrome-linked-list/solution.java
@@ -1,17 +1,20 @@
-    public boolean bruteForceMethod2(ListNode head){
-        List<Integer> list = new ArrayList<>();
-        ListNode temp = head;
-        while(temp != null){
-            list.add(temp.val);
-            temp = temp.next;
-        }
-        int l = 0;
-        int r = list.size() - 1;
-        while(l <= r){
-            if(list.get(l) != list.get(r)){
-                return false;
-            }
-        }
-        return true;
-            l++;
-            r--;
+     */
+        - if all matched then return true
+        - reverse the list from next node after middle till end and get the new head
+        - now will compare each val from head with new head if any val is not matched then return false
+        - will find the middle node
+    /**
+    }
+        return palindromOptimal(head);
+    public boolean isPalindrome(ListNode head) {
+class Solution {
+ */
+ * }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    public boolean palindromOptimal(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        // get the middle
+        while (fast != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
