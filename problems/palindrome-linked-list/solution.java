0abb46f8c5dfd969388b4eafff7ad3f295956d52
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-17 20:06:14 +0530

    Sync leetcode submission: palindrome-linked-list

--- problems/palindrome-linked-list/solution.java
+++ problems/palindrome-linked-list/solution.java
@@ -2,17 +2,17 @@
         - now will compare each val from head with new head if any val is not matched then return false
         - will find the middle node
     /**
-    }
-        return palindromOptimal(head);
-    public boolean isPalindrome(ListNode head) {
-class Solution {
- */
- * }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- *     ListNode(int val) { this.val = val; }
- *     ListNode() {}
- *     ListNode next;
- *     int val;
- * Definition for singly-linked list.
- * public class ListNode {
-/**
+        - if all matched then return true
+     */
+    public boolean palindromOptimal(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        // get the middle
+        while (fast != null && fast.next && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        // slow will be at middle and fast will be at the last - 1
+        ListNode newHead = reverseLL(slow.next);
+        ListNode temp = head;
+        while (newHead != null) {
