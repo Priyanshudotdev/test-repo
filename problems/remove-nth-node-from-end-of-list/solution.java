d7582913619a3f9b28408baef0b838e9f012faeb
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-18 20:11:06 +0530

    Sync leetcode submission: remove-nth-node-from-end-of-list

--- problems/remove-nth-node-from-end-of-list/solution.java
+++ problems/remove-nth-node-from-end-of-list/solution.java
@@ -1,17 +1,17 @@
-        for (int i = 0; i < n - 1; i++) {
-        ListNode temp = dummy;
-        dummy.next = newHead;
         ListNode dummy = new ListNode(0);
-        ListNode newHead = reverse(head);
-        if (head == null) return null;
-    public static ListNode bruteForceMethod(ListNode head, int n) {
-    }
-        return bruteForceMethod(head, n);
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-class Solution {
- */
- * }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- *     ListNode(int val) { this.val = val; }
-            temp = temp.next;
+        dummy.next = head;
+        ListNode left = dummy;
+        ListNode right = head;
+        for (int i = 0; i < n; i++) {
+            right = right.next;
         }
+        while (right != null) {
+            left = left.next;
+            right = right.next;
+        }
+        left.next = left.next.next;
+        return dummy.next;
+    }
+    public static ListNode bruteForceMethod(ListNode head, int n) {
+        if (head == null) return null;
+        ListNode newHead = reverse(head);
