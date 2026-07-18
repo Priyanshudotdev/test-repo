420a48192c8837c05819df365b5e1f21a8aaf87b
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-18 21:35:11 +0530

    Sync leetcode submission: sort-list

---
+++ problems/sort-list/solution.java
@@ -0,0 +1,17 @@
+        List<Integer> list = new ArrayList<>();
+        if(head == null) return null;
+    public static ListNode sortLL(ListNode head) {
+    }
+    public ListNode sortList(ListNode head) {
+        return sortLL(head);
+class Solution {
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode() {}
+ *     ListNode next;
+ *     int val;
+ * public class ListNode {
+ * Definition for singly-linked list.
+/**
