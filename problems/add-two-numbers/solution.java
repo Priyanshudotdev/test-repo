5435a04bc15ac71df6ca0e0173b9701561f414c8
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-19 18:36:02 +0530

    Sync leetcode submission: add-two-numbers

--- problems/add-two-numbers/solution.java
+++ problems/add-two-numbers/solution.java
@@ -1,14 +1,16 @@
-    public List<Integer> getDigitsOfNum(int num) {
-        List<Integer> list = new ArrayList<>();
-        if (num == 0) {
-            list.add(0);
-            return list;
-        }
-        while (num > 0) {
-            int digit = num % 10;
-            list.add(digit);
-            num /= 10;
-        }
-        return list;
     }
-}
+    public ListNode addTwoNum(int num) {
+        ListNode head = null;
+        return addTwoNum(reverseNum(num1 + num2));
+        int num2 = reverseLL(l2);
+        int num1 = reverseLL(l1);
+        if (l1 == null && l2 == null) return null;
+    public ListNode bruteForceMethod(ListNode l1, ListNode l2) {
+    }
+        return bruteForceMethod(l1,l2);
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+class Solution {
+ */
+ * }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     ListNode(int val) { this.val = val; }
