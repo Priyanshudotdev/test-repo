33241731f13ecf70b2ea1a101a5e3c20e22f5f4c
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-19 18:48:07 +0530

    Sync leetcode submission: add-two-numbers

--- problems/add-two-numbers/solution.java
+++ problems/add-two-numbers/solution.java
@@ -1,16 +1,13 @@
+    public static int reverseNum(int num) {
+        int sign = num < 0 ? -1 : 1;
+        String reversedStr = new StringBuilder(String.valueOf(Math.abs(num)))
+            .reverse()
+            .toString();
+        return Integer.parseInt(reversedStr) * sign;
     }
-    public ListNode addTwoNum(int num) {
-        ListNode head = null;
-        return addTwoNum(reverseNum(num1 + num2));
-        int num2 = reverseLL(l2);
-        int num1 = reverseLL(l1);
-        if (l1 == null && l2 == null) return null;
-    public ListNode bruteForceMethod(ListNode l1, ListNode l2) {
+}
+            prev = temp;
+            temp = next;
+        }
+        return reverseNum(num);
     }
-        return bruteForceMethod(l1,l2);
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-class Solution {
- */
- * }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- *     ListNode(int val) { this.val = val; }
