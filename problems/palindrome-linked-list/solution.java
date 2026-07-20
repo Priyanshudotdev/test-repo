fa66964118e1304f9e19335b24ba38449a8fb009
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-20 10:07:27 +0530

    Sync leetcode submission: palindrome-linked-list

--- problems/palindrome-linked-list/solution.java
+++ problems/palindrome-linked-list/solution.java
@@ -1,17 +1,17 @@
-        // slow will be at middle and fast will be at the last - 1
-        ListNode newHead = reverseLL(slow.next);
-        ListNode temp = head;
-        while (newHead != null) {
-            if (newHead.val != temp.val) {
-                return false;
-            }
-            newHead = newHead.next;
-            temp = temp.next;
-        }
-                reverseLL(newHead);
-        }
             slow = slow.next;
-        while (fast.next != null && fast.next.next != null) {
-            fast = fast.next.next;
-        // get the middle
-        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+        ListNode fast = dummy.next.next;
+        ListNode slow = dummy.next;
+        ListNode dummy = new ListNode(0, head);
+    public boolean reSolve(ListNode head) {
+    }
+        return reSolve(head);
+    public boolean isPalindrome(ListNode head) {
+class Solution {
+ */
+ * }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode() {}
+ *     ListNode next;
+ *     int val;
