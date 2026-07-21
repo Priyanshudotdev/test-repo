5d770c3d2974f9ec7f652e42aec7a7773828aaf9
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-21 12:12:26 +0530

    Sync leetcode submission: remove-duplicates-from-sorted-list

---
+++ problems/remove-duplicates-from-sorted-list/solution.java
@@ -0,0 +1,18 @@
+        return bruteForceMethod(head);
+    }
+    public static ListNode bruteForceMethod(ListNode head){
+        ListNode temp = head;
+        while(temp.next != null){
+    public ListNode deleteDuplicates(ListNode head) {
+class Solution {
+ */
+            ListNode nextNode = temp.next;
+            if(temp.val == nextNode.val){
+                // delete the next node
+                temp.next = temp.next.next;
+                continue;
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
