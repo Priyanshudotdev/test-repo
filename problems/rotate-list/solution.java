abcdf2299cf9e6a966d5e96f6c1ef3adf4421ffb
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-22 12:04:31 +0530

    Sync leetcode submission: rotate-list

---
+++ problems/rotate-list/solution.java
@@ -0,0 +1,17 @@
+        int n = length(head);
+        if (head == null || head.next == null || k == 0) return head;
+    public ListNode bruteForceMethod(ListNode head, int k) {
+    }
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        return bruteForceMethod(head,k);
+ * }
+ */
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ * public class ListNode {
+ * Definition for singly-linked list.
+/**
