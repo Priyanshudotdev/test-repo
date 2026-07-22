9d4e174d07fae609a3de7b3ee7af63f534452b38
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-22 15:08:53 +0530

    Sync leetcode submission: implement-queue-using-stacks

---
+++ problems/implement-queue-using-stacks/solution.java
@@ -0,0 +1,17 @@
+        while(!first.isEmpty()){
+    public int pop() {
+    
+    }
+        first.push(x);
+    public void push(int x) {
+    }
+    
+        first = new Stack<>();
+        second = new Stack<>();
+    public MyQueue() {
+    private Stack<Integer> second;
+    int size = 0;
+    
+    private Stack<Integer> first;
+    private int[] data;
+class MyQueue {
