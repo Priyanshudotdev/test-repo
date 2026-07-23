3d9d331cf973a62f2e9c580b67d458731e38c7b0
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-23 23:16:18 +0530

    Sync leetcode submission: backspace-string-compare

---
+++ problems/backspace-string-compare/solution.java
@@ -0,0 +1,16 @@
+    public String formatString(String s){
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(!st.isEmpty() && s.charAt(i) == '#'){
+                st.pop();
+            }else{
+                st.push(s.charAt(i));
+            }
+    }
+        return result.equals(formatString(t));
+        String result = formatString(s);
+    public boolean bruteForceMethod(String s, String t){
+    }
+        return bruteForceMethod(s, t);
+    public boolean backspaceCompare(String s, String t) {
+class Solution {
