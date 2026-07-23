ae5f0820df50bcddb9fe4e4978f24e5d0e833e9d
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-23 08:39:09 +0530

    Sync leetcode submission: next-greater-element-i

--- problems/next-greater-element-i/solution.java
+++ problems/next-greater-element-i/solution.java
@@ -1,17 +1,19 @@
-        int k = 0;
-        for(int i = 0; i < nums1.length; i++){
-            result[k++] = peek(nums2,nums1[i]);
         }
-    }
-    public int peek(int[] nums, int num){
-        for(int i = 0; i + 1 < nums.length; i++){
-            if(nums[i] == num){
-                for(int j = i; j < nums.length; j++){
-        return result;
-                    if(nums[j] > num){
+        // loop till num2
+        for(int i = 0; i < nums2.length; i++){
+            if(map.get(nums2[i]) == null) continue;
+            for(int j = i + 1; j < nums2.length; j++){
+                int idx = map.getOrDefault(nums2[i], -1);
+                    result[idx] = nums2[j];
+                    break;
                 }
-                        return nums[j];
-                    }
+            map.put(nums1[i], i);
+        for(int i = 0; i < nums1.length; i++){
+                if(nums2[j] > nums2[i] && idx != -1){
+        // put in map with its index values
+        Arrays.fill(result, -1);
+        // make all elements to -1 (default)
+        int[] result = new int[nums1.length];
             }
         }
-        return -1;
+        return result;
