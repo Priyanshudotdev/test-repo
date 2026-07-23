f2310019338af9a8a08a467a9f8cfefde146fa8b
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-23 12:29:20 +0530

    Sync leetcode submission: sum-of-subarray-minimums

--- problems/sum-of-subarray-minimums/solution.java
+++ problems/sum-of-subarray-minimums/solution.java
@@ -5,13 +5,13 @@
     }
                 int min = getMin(Arrays.copyOfRange(arr,i, j+1));
             for(int j = i + 1; j < arr.length; j++){
-            if(st.isEmpty()){
-        for(int i = 0; i < arr.length; i++){
-        int sum = 0;
-    public int sumSubarrayMins(int[] arr) {
-class Solution {
-        Stack<Integer> st = new Stack<>();
-                st.push(arr[i]);
             }
                 st.push(st.pop() + arr[i]);
             }else{
+            if(st.isEmpty()){
+                st.push(arr[i]);
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < arr.length; i++){
+        int sum = 0;
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
