22bfcfc81a0e4aa858598ad9537510a5386fd0ed
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-23 16:53:29 +0530

    Sync leetcode submission: valid-parentheses

--- problems/valid-parentheses/solution.java
+++ problems/valid-parentheses/solution.java
@@ -1,18 +1,19 @@
-        map.put('}', 2);
-        map.put('[', -3);
-        map.put(']', 3);
+        closeMap.put('}', 2);
+        startMap.put('[', -3);
+        closeMap.put(']', 3);
         for(int i = 0; i < s.length(); i++){
-            if(!st.isEmpty() && map.get(st.peek()) != null && map.get(s.charAt(i)) != null){
-                int s1 = map.get(st.peek());
+            if(!st.isEmpty() && startMap.get(st.peek()) != null && closeMap.get(s.charAt(i)) != null){
+                int s1 = startMap.get(st.peek());
             }else{
-                int s2 = map.get(s.charAt(i));
+                int s2 = closeMap.get(s.charAt(i));
                 if(s1 + s2 == 0) st.pop();
                 else st.push(s.charAt(i));
-                st.push(s.charAt(i));
-        map.put('{', -2);
-        map.put(')', -1);
-        map.put('(', 1);
+        startMap.put('{', -2);
+        closeMap.put(')', -1);
+        startMap.put('(', 1);
         Stack<Character> st = new Stack<>();
-        Map<Character, Integer> map = new HashMap<>();
-class Solution {
-    public boolean isValid(String s) {
+                st.push(s.charAt(i));
+            }
+        }
+        boolean result = st.isEmpty() ? true: false;
+        return result;
