29275b131a432cce3c211e3c849cb2ea1793588a
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-23 19:00:31 +0530

    Sync leetcode submission: decode-string

--- problems/decode-string/solution.java
+++ problems/decode-string/solution.java
@@ -1,19 +1,16 @@
+import java.util.Stack;
+class Solution {
+    public static String decodeString(String s) {
+        Stack<Character> st = new Stack<>();
+        String result = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ']') {
+                st.push(s.charAt(i));
+            } else {
+                String subStr = "";
+                while (st.peek() != '[') {
+                    subStr = st.pop() + subStr;
+                }
+                st.pop();
                 int repeatCount = st.pop() - '0';
                 while (!st.isEmpty() && Character.isDigit(st.peek())) {
-                    repeatCount = repeatCount * 10 + (st.pop() - '0');
-                }
-                String newStr = subStr.repeat(repeatCount);
-                for (int j = 0; j < newStr.length(); j++) {
-                    st.push(newStr.charAt(j));
-                }
-            }
-        }
-        while (!st.isEmpty()) {
-            result = st.pop() + result;
-        }
-        return result;
-    }
-    // public String multiplyStr(String str, int k){
-    //     String result = "";
-    //     for(int i = 0; i < k; k++){
-    //         result = result + str;
