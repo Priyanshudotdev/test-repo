af441424981b47683079c4b2cdb68b419bc640d3
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-24 15:29:20 +0530

    Sync leetcode submission: product-of-array-except-self

--- problems/product-of-array-except-self/solution.java
+++ problems/product-of-array-except-self/solution.java
@@ -1,19 +1,19 @@
-            for(int j = i + 1; j < n; j++){
-            // for right
-            for(int j = 0; j < i; j++){
-                result[i] *= nums[j];
-            }
-            // for left
-            int lmul = 1;
-            int rmul = 1;
-        for(int i = 0; i < n; i++){
-                result[i] *= nums[j];
-            }
+        // for right
+        for(int i = n - 1; i >= 0; i--){
+        for(int i = 1; i < n; i++){
+            result[i] = result[i-1] * nums[i - 1];
+        }
+        result[0] = 1;
+        int rightProduct = 1;
+        // for left
+        int[] result = new int[n];
+        int n = nums.length;
+    public int[] optimalMethod(int[] nums){
+     */
+        - now again loop but this time for right side multiplication
+            result[i] *= rightProduct;
+            rightProduct *= nums[i]; 
         }
         return result;
     }
     /**
-        Brute Force Solution
-        - simply will multiply elements from left side except current one and store it
-        - and same for right side except current one and store it
-        - then will multiple both left and right side result and store it in current index
