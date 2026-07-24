c0a277ca997bc4a3d503cbfb8b8987cc2f25589d
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-24 11:07:19 +0530

    Sync leetcode submission: maximum-subarray

---
+++ problems/maximum-subarray/solution.java
@@ -0,0 +1,22 @@
+            - Resolve
+            - take two vars : one to store the sum and one to store max sum (result)
+            - check if currSum is less than zero or not
+        /*
+    public int optimalSol(int[] nums){
+    }
+        
+        return sum;
+                tempSum = 0;
+            }
+        }
+            tempSum += nums[i];
+            sum = Math.max(tempSum, sum);
+            if(tempSum < 0){
+        int sum = 0;
+        int tempSum = 0;
+        for(int i = 0; i < nums.length; i++){
+    public int reSolve(int[] nums){
+    }
+        return reSolve(nums);
+    public int maxSubArray(int[] nums) {
+class Solution {
