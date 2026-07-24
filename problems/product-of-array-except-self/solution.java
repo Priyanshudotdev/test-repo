4dbafed4f62c2109ff6b4154387c21f224342cb6
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-24 15:20:31 +0530

    Sync leetcode submission: product-of-array-except-self

--- problems/product-of-array-except-self/solution.java
+++ problems/product-of-array-except-self/solution.java
@@ -9,13 +9,10 @@
             int rmul = 1;
         for(int i = 0; i < n; i++){
             int lmul = 1;
-        int[] result = new int[nums.length];
-        int n = nums.length;
-    public int[] optimalMethod(int[] nums){
-        - at last just multiple left*right and store it in result array
-     */
-        - same for right
-        - left array where all elements left answers will be store
-        - create two arrays to store multiplication result 
-        - Optimal Method
+            // left[i] = lmul;
+            // right[i] = rmul;
+            result[i] = lmul * rmul; 
+        }
+        return result;
+    }
     /**
