3ee617f32420c32c6dc408053164bb3b5d4c1922
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-24 19:42:08 +0530

    Sync leetcode submission: continuous-subarray-sum

--- problems/continuous-subarray-sum/solution.java
+++ problems/continuous-subarray-sum/solution.java
@@ -11,12 +11,13 @@
         for(int i = 0; i < nums.length; i++){
             total += nums[i];
             int r = total % k;
-            if(map.get(r) == null){
-                map.put(r, i);
-            }else if(map.get(r) != null && i -  map.get(r) > 1){
-                return true;
-            }
-        }
-        return false;
-    }
-}
+        map.put(0, -1);
+                now notice here that we have two remainder with same value : that means 
+                - 23+2 % 6 => 1 : map({5,0},{1,1})
+                - 25+4 % 6 => 5 : map({5,0}, {1,1}, {5,2})
+                - 23 % 6 => 5 : map({5,0})
+        : so lets take the given example -> [23,2,4,6,7] k = 6
+        will take each number and mod it with k and store the ans to hasmap with its index
+        - before that we need to understand the logic behind it
+        - init a remainder hashmap and a total var (where will store the prefix sum)
+        Optimal Approach
