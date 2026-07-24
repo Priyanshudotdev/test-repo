bf7952f056761d59b9ac7497fc1202e06265bd50
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-24 15:19:43 +0530

    Sync leetcode submission: product-of-array-except-self

--- problems/product-of-array-except-self/solution.java
+++ problems/product-of-array-except-self/solution.java
@@ -1,23 +1,21 @@
-        int mul = 0;
-        int[] ans = new int[nums.length];
-        System.out.println();
-        for(int i = 0; i < nums.length; i++){
-            int leftstart = 0;
-            int rightstart = i + 1;
-            
-            int left = multiplyArr(Arrays.copyOfRange(nums, leftstart, i));
-            int right = multiplyArr(Arrays.copyOfRange(nums, rightstart , nums.length ));
-            
-            ans[i] = left * right;
-        }
-        return ans;
-    }
-    
-    public int multiplyArr(int[] nums){
-        int mul = 1;
-        for(int num: nums){
-            mul *= num;
-        }
-        return mul;
-    }
-}
+            for(int j = 0; j < i; j++){
+                lmul *= nums[j];
+            }
+            // for right
+            for(int j = i + 1; j < n; j++){
+                rmul *= nums[j];
+            }
+            // for left
+            int rmul = 1;
+        for(int i = 0; i < n; i++){
+            int lmul = 1;
+        int[] result = new int[nums.length];
+        int n = nums.length;
+    public int[] optimalMethod(int[] nums){
+        - at last just multiple left*right and store it in result array
+     */
+        - same for right
+        - left array where all elements left answers will be store
+        - create two arrays to store multiplication result 
+        - Optimal Method
+    /**
