2cad91d4f5c73c9e61e3b65f906729c9ee379ce2
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-24 08:54:03 +0530

    Sync leetcode submission: move-zeroes

--- problems/move-zeroes/solution.java
+++ problems/move-zeroes/solution.java
@@ -1,18 +1,19 @@
-    public void reresolve(int[] nums){
-        int i = 0;
-        int j = 1;
-        
-        while(j < nums.length){
+            }else if(nums[i] != 0){
+                i++;
+                nums[j] = temp;
+                nums[i] = nums[j];
             if(nums[i] == 0 && nums[j] != 0){
                 int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
-            }
-            j++;
-        }
+        
+        while(j < nums.length){
+        int i = 0;
+        int j = 1;
+    public void reresolve(int[] nums){
     }
          reresolve(nums);
     public void moveZeroes(int[] nums) {
 class Solution {
-            i++;
+                i++;
+            }
+            j++;
+        }
