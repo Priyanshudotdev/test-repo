7c57fa1ca09363b126eb7e1851594160ee1a3a75
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-24 16:05:58 +0530

    Sync leetcode submission: subarray-sum-equals-k

---
+++ problems/subarray-sum-equals-k/solution.java
@@ -0,0 +1,19 @@
+        }
+            }
+                }
+                    result++;
+                if(sum == k){
+                sum += nums[j];
+            for(int j = i; j < nums.length; j++){
+        return result;
+    }
+            int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+        int result = 0;
+    public int reSolve(int[] nums, int k){
+        return reSolve(nums, k);   
+    }
+    public int subarraySum(int[] nums, int k) {
+class Solution {
+import java.util.ArrayList;
+import java.util.List;
