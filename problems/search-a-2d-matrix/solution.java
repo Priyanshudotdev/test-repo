45adfa6d095681bc64c7f7fa8a4144fecc61d065
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-25 12:25:50 +0530

    Sync leetcode submission: search-a-2d-matrix

---
+++ problems/search-a-2d-matrix/solution.java
@@ -0,0 +1,22 @@
+        if (top > bottom) return false;
+            }
+        }
+            } else if (target < arr[mid][0]) {
+                bottom = mid - 1;
+            } else {
+                break;
+            if (target > arr[mid][cols - 1]) {
+                top = mid + 1;
+        while (top <= bottom) {
+            mid = top + (bottom - top);
+        int top = 0;
+        int bottom = rows - 1;
+        int mid = 0;
+        int cols = arr[0].length;
+    public boolean optimalMethod(int[][] arr, int target){
+        int rows = arr.length;
+    }
+        
+        return optimalMethod(matrix, target);
+    public boolean searchMatrix(int[][] matrix, int target) {
+class Solution {
