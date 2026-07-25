7635ec29eca426748cc344b89da8b8c220dadfee
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-25 08:34:29 +0530

    Sync leetcode submission: find-first-and-last-position-of-element-in-sorted-array

--- problems/find-first-and-last-position-of-element-in-sorted-array/solution.java
+++ problems/find-first-and-last-position-of-element-in-sorted-array/solution.java
@@ -2,15 +2,18 @@
             
             if(nums[mid] == target){
                 // check for left and right
-                if(nums[mid - 1] == target){
+                if(nums[mid - 1] >= 0 && nums[mid - 1] == target){
                     return new int[]{mid - 1, mid};
                 }
-                if(nums[mid + 1] == target){
+                if(nums[mid + 1] < nums.length && nums[mid - 1] == target){
                     return new int[]{mid,mid+1};
                 }
             }else if(nums[mid] > target) high = mid - 1;
             else if(nums[mid] < target) low = mid + 1;
-        }
+        while(low <= high){
         
-        return new int[]{-1,-1};
-    }
+        int high = nums.length - 1;
+        int low = 0;
+        
+        }
+            return new int[]{-1,-1};
