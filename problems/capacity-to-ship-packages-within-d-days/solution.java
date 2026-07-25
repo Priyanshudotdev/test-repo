a3ca8b1c5089dced4e6a5d04affc917ea2eded72
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-25 17:06:50 +0530

    Sync leetcode submission: capacity-to-ship-packages-within-d-days

--- problems/capacity-to-ship-packages-within-d-days/solution.java
+++ problems/capacity-to-ship-packages-within-d-days/solution.java
@@ -1,19 +1,17 @@
-            for (int num : weights) {
-                ct += num;
-            int ct = 0;
-            System.out.println("mid :" + mid);
-            int d = 0;
+            if (canShip(weights, days, mid)) {
+                result = mid;
+                high = mid - 1;
             int mid = low + (high - low) / 2;
         while (low <= high) {
-        int result = Integer.MAX_VALUE;
-        int high = maxWeight;
+        int result = totalWeight;
+        int low = maxWeight;
+        int high = totalWeight;
         // binary search goes from here
-        int low = maxNum;
-            maxNum = Math.max(maxNum, num);
+            totalWeight += num;
         }
-            maxWeight += num;
         for (int num : weights) {
-        int maxNum = 0;
+            maxWeight = Math.max(maxWeight, num);
         int maxWeight = 0;
-    public static int shipWithinDays(int[] weights, int days) {
+        int totalWeight = 0;
 class Solution {
+    public static int shipWithinDays(int[] weights, int days) {
