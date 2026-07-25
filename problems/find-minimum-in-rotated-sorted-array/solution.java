9424571d37d8753c30f404447cabc7dc1be9c546
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-25 09:24:02 +0530

    Sync leetcode submission: find-minimum-in-rotated-sorted-array

--- problems/find-minimum-in-rotated-sorted-array/solution.java
+++ problems/find-minimum-in-rotated-sorted-array/solution.java
@@ -1,14 +1,12 @@
-                - else move high to mid - 1
-                - if num of low is smaller than current num which means we need to move the low to mid + 1
-            - else will check the current num with num of low : 
-            respect to current num
-            - if current num is greater than right side num (that means its the last largest number) will return next num 
-            - if current num is greater than left side num (that means it is in sorted order) will move forward
-            - will just use b search for finding the breaking point 
-            - will check for current num with its left and right side nums
-    /**
-        - Optimal Approach
+    public int reSolve(int[] nums){
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(mid > 0 && nums[mid] < nums[mid - 1]) return nums[mid];
+            if(mid < nums.length - 1 && nums[mid] > nums[mid + 1]) return nums[mid + 1];
+            if(nums[low] < nums[mid]) low = mid + 1;
+            else high = mid - 1;
+        }
+        return nums[0];
     }
-        return reSolve(nums);
-    public int findMin(int[] nums) {
-class Solution {
