3f3838b37114c652b91c0d54933e483d150a127d
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-25 09:54:05 +0530

    Sync leetcode submission: search-in-rotated-sorted-array-ii

--- problems/search-in-rotated-sorted-array-ii/solution.java
+++ problems/search-in-rotated-sorted-array-ii/solution.java
@@ -1,19 +1,19 @@
-                    if (target < nums[mid]) {
-                        high = mid - 1;
-                if (nums[low] <= target && target < nums[mid]) {
-            // left porttion
-            if (nums[low] < nums[mid]) {
-            }
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                return true;
-        while (low <= high) {
-        }
-        if (low > high || nums == null || nums.length == 0) {
-            return false;
-    public static boolean bSearch(int[] nums, int target, int low, int high) {
-        return bSearch(nums, target, 0, nums.length - 1);
+    public static boolean reBSearch(
     }
+        return result;
+            return reBSearch(nums, target, i, nums.length - 1);
+        }
+        if (!result) {
+        boolean result = reBSearch(nums, target, 0, i - 1);
+        // System.out.println("i : " + i);
+        }
+            if (nums[i] < nums[i - 1]) break;
+        // find the breaking point
+        int i = 0;
+        for (i = nums.length - 1; i - 1 >= 0; i--) {
+    public static boolean reSolve2(int[] nums, int target) {
+    }
+        // return bSearch(nums, target, 0, nums.length - 1);
+        return reSolve2(nums, target);
     public static boolean search(int[] nums, int target) {
 class Solution {
-                    }
