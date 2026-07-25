0b431835e5ecfcab6c351ec797abcf985a85c914
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-25 08:46:16 +0530

    Sync leetcode submission: find-first-and-last-position-of-element-in-sorted-array

--- problems/find-first-and-last-position-of-element-in-sorted-array/solution.java
+++ problems/find-first-and-last-position-of-element-in-sorted-array/solution.java
@@ -1,17 +1,17 @@
-            int mid = low + (high - low)/2;
-        while(low <= high){
-        
-        int high = nums.length - 1;
-        int low = 0;
-        
-        }
-            return new int[]{0,0};
-        }else if(nums.length == 1){
-            return new int[]{-1,-1};
-        if(nums.length == 0){
-    static int[] reSove2(int[] nums, int target){
     }
-        return reSove2(nums,target);
-    static int[] searchRange(int[] nums, int target){
-  
-class Solution {
+    static int bSearch2(int[] nums, int target, boolean flag){
+        int low = 0;
+        int high = nums.length - 1;
+        int result = -1;
+        return new int[]{index1, index2};
+        int index2 = bSearch2(nums, target, false);
+        int index1 = bSearch2(nums, target, true);
+    static int[] reSove2(int[] nums, int target){
+        index or 2nd"
+     */
+        - but how do we know when we have to do first index or second ?
+        - wil init a flag boolean var which will tell the b search that "search for 1st 
+        - one for first index and second for second index
+        - will do two b search
+        - Optimal Solution
+    /**
