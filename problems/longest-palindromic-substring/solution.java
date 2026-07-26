74e335bc392fa00ab7cff035af46c482afe82fce
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-26 15:32:51 +0530

    Sync leetcode submission: longest-palindromic-substring

--- problems/longest-palindromic-substring/solution.java
+++ problems/longest-palindromic-substring/solution.java
@@ -1,17 +1,18 @@
-                l--;
-                    result = s.substring(l, r+1);
+            }
                 }
-            while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)){
-                int currentLen = r - l + 1;
-                if(currentLen > resultLen){
-                    resultLen = currentLen;
-            int r = i;
-            // odd resulting
+                l--;
+                r++;
+                int currLen = r - l + 1;
+                if (currLen > maxLen) {
+                    maxLen = currLen;
+                    result = s.substring(l, r + 1);
             int l = i;
-        for(int i = 0; i < s.length(); i++){
+            int r = i;
+            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+        for (int i = 0; i < s.length(); i++) {
+            // odd string
         String result = "";
-        int resultLen = 0;
+        int maxLen = 0;
         if(s == null || s.length() == 0) return "";
-        // StringBuilder str = new StringBuilder();
     public String longestPalindrome(String s) {
 class Solution {
