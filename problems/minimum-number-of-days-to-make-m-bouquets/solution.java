21766874ad25def7dde9241ae0506bffa0f397d5
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-26 12:36:39 +0530

    Sync leetcode submission: minimum-number-of-days-to-make-m-bouquets

--- problems/minimum-number-of-days-to-make-m-bouquets/solution.java
+++ problems/minimum-number-of-days-to-make-m-bouquets/solution.java
@@ -1,14 +1,14 @@
+class Solution {
+    public static int minDays(int[] arr, int m, int k) {
         return reSolve(arr, m,k);
     }
     public static int reSolve(int[] arr, int m, int k){
         if (arr.length < m * k) return -1;
         // m -> no of bouquets needed
         // k -> no of flowers required for each bouquets
-        int low = 1;
+        int low = Integer.MAX_VALUE;
         int high = Integer.MIN_VALUE;
         for (int num : arr) {
             low = Math.min(low, num);
             high = Math.max(high, num);
         }
-    public static int minDays(int[] arr, int m, int k) {
-class Solution {
