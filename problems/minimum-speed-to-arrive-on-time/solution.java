e21a773527bc2d1896899123d9443a672778cf02
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-26 09:41:15 +0530

    Sync leetcode submission: minimum-speed-to-arrive-on-time

--- problems/minimum-speed-to-arrive-on-time/solution.java
+++ problems/minimum-speed-to-arrive-on-time/solution.java
@@ -1,17 +1,22 @@
-class Solution {
-    public int minSpeedOnTime(int[] dist, double hour) {
-        int low = Integer.MAX_VALUE;
-        int high = Integer.MIN_VALUE;
-        for (int num : dist) {
-            low = Math.min(low, num);
-            high = Math.max(high, num);
-        }
-        int result = Integer.MAX_VALUE;
+            // add element
+            time += (double) dist[dist.length - 1] / mid;
+            }
+                time += Math.ceil((double) dist[i] / mid);
+            for (int i = 0; i < dist.length - 1; i++) {
+            double time = 0;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            double hrsNeeded = 0;
-            for (int i = 0; i < dist.length; i++) {
-                double hr = dist[i] / (float) mid;
-                if (i != dist.length - 1) {
-                    hrsNeeded += Math.ceil(hr > 0.0 ? hr : 1);
-                } else {
+        int low = 1;
+        int high = 10_000_000;
+        if (hour <= dist.length - 1) return -1;
+    public static int minSpeedOnTime(int[] dist, double hour) {
+class Solution {
+            if (time <= hour) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
