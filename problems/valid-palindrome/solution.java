e937ef8dcd49ea53153ade20139fe5d6e1544a50
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-26 12:57:47 +0530

    Sync leetcode submission: valid-palindrome

--- problems/valid-palindrome/solution.java
+++ problems/valid-palindrome/solution.java
@@ -3,15 +3,14 @@ class Solution {
         return reSolve(s);
     }
     public boolean reSolve(String s){
-        String str = s.replaceAll("[\\s,./@#:]+", "").trim().toLowerCase();
+        String str = s.replaceAll("[^a-zA-Z0-9]+", "").trim().toLowerCase();
         return new StringBuilder(str).reverse().toString().equals(str);
     }
     public boolean optimal(String s){
-        s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        s = s.replaceAll("[^A-Za-z0-9]+", "").toLowerCase();
         String p = new StringBuilder(s).reverse().toString();
         if (s.equals(p)) {
             return true;
         }
         return false;
     }
-}
