32dd0b0bb1fa9870c52338d31fc919f37e57456e
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-27 13:10:04 +0530

    Sync leetcode submission: maximum-average-subarray-i

---
+++ problems/maximum-average-subarray-i/solution.java
@@ -0,0 +1,15 @@
+            }
+                sum += nums[j];
+            double sum = 0;
+            for(int j = l; j <= i; j++){
+            maxAvg = Math.max(maxAvg, sum/(double) k);
+            l++;
+        }
+        return maxAvg;        
+    }
+        for(int i = k - 1; i < nums.length; i++){
+    double maxAvg = 0;
+    int l = 0;
+}
+    public double findMaxAverage(int[] nums, int k) {
+class Solution {
