782f33c47bbf4cf1379766d55a5131a386d857bf
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-27 08:17:34 +0530

    Sync leetcode submission: permutation-in-string

--- problems/permutation-in-string/solution.java
+++ problems/permutation-in-string/solution.java
@@ -1,21 +1,18 @@
-class Solution {
-    public boolean checkInclusion(String s1, String s2) {
-        List<Integer> list = new ArrayList<>();
-        
-        for(int i = 0; i < s1.length(); i++){
-            int index = s2.indexOf(s1.charAt(i));
-            list.add(index);
-        }
-        
-        list.sort((a,b) -> a - b);
-        
-        int count = list.get(0);
-        for(int i = 1; i < list.size(); i++){
-            count++;
-            if(list.get(i) != count) {
-                return false;
+                freq2[s2.charAt(left) - 'a']--;
+                left++;
+            }
+            
+            if(right - left + 1 == s1.length()){
+                boolean isMatch = true;
+                for(int i = 0; i < 26; i++){
+                    if(freq1[i] != freq2[i]){
+                        isMatch = false;
+                        break;
+                    }
+                }
+                if(isMatch) return true;
             }
         }
-        
-        // System.out.println(true);
-        return true;
+        return false;
+    }
+   
