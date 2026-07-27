207c362b55326388dde9a21e40bf3dfd4d8c4f78
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-27 10:14:12 +0530

    Sync leetcode submission: is-subsequence

---
+++ problems/is-subsequence/solution.java
@@ -0,0 +1,17 @@
+        for(int i = 0; i < s.length(); i++){
+            charArr[s.charAt(i) - 'a']++;
+        }
+                charArr[t.charAt(i) - 'a']--;
+        for(int i = 0; i < t.length(); i++){
+            if(s.indexOf(t.charAt(i)) >= 0){
+            }
+        }
+        for(int num: charArr){
+            if(num != 0) return false;
+        }
+        return true;
+        int[] charArr = new int[26];
+    }
+}
+    public boolean isSubsequence(String s, String t){
+class Solution {
