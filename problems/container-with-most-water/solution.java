f2062c857d16f929cf214f2e9532738b267702a7
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-27 16:58:49 +0530

    Sync leetcode submission: container-with-most-water

--- problems/container-with-most-water/solution.java
+++ problems/container-with-most-water/solution.java
@@ -1,19 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int maxArea = Integer.MIN_VALUE;
+        while(l < r && l < height.length && r >= 0){
             // find the area
             int area = (r-l) * Math.min(height[l], height[r]);
             maxArea = Math.max(area, maxArea);
             if(height[l] < height[r]){
                 l++;
-            }else if(height[l] == height[r] && height[l+1] < height[r-1]){
-                l++;
+            // }else if(height[l] == height[r] && height[l+1] < height[r-1]){
+                // l++;
             }else{
                 r--;
             }
         }
         return maxArea;
     }
-        while(l < r && l < height.length && r >= 0){
-        int maxArea = Integer.MIN_VALUE;
-        int r = height.length - 1;
-        int l = 0;
-    public int maxArea(int[] height) {
-class Solution {
