e16a74e59cbb13f0e992f07651bf7b798c260c5e
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-27 08:21:10 +0530

    Sync leetcode submission: permutation-in-string

--- problems/permutation-in-string/solution.java
+++ problems/permutation-in-string/solution.java
@@ -3,16 +3,16 @@
             }
             
             if(right - left + 1 == s1.length()){
-                boolean isMatch = true;
-                for(int i = 0; i < 26; i++){
-                    if(freq1[i] != freq2[i]){
-                        isMatch = false;
-                        break;
-                    }
-                }
-                if(isMatch) return true;
-            }
+            if(right - left + 1 > s1.length()){
+            freq2[s2.charAt(right) - 'a']++;
+        for(int right = 0; right < s2.length(); right++){
+        int left = 0;
+            freq1[s -'a']++;
         }
-        return false;
-    }
-   
+        for(char s : s1.toCharArray()){
+        int[] freq2 = new int[26];
+        
+        int[] freq1 = new int[26];
+        if(s1.length() > s2.length()) return false;
+    public boolean checkInclusion(String s1, String s2) {
+class Solution {
