ee6d3351341a0491c87f4f2d691c1bd80f1a6bfd
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-27 10:55:36 +0530

    Sync leetcode submission: longest-substring-without-repeating-characters

--- problems/longest-substring-without-repeating-characters/solution.java
+++ problems/longest-substring-without-repeating-characters/solution.java
@@ -1,19 +1,19 @@
-        - will check each string and if we got any duplicate string will 
-        from the element from that substring
-        - why ? : let say : s = "abcabcbb", here we reach till c and our 
-        substring looks like "abc" so will remove all elements til we get 
-        out first "a" 
-     */
-    public static int optimalMethod(String s){
-        Set<Character> set = new HashSet<>();
-        int result = 0;
-        int l = 0;
-        for (int i = 0; i < s.length(); i++) {
-            while (set.contains(s.charAt(i))) {
-                set.remove(s.charAt(l));
-                l += 1;
+            if(map.getOrDefault(leftChar, -1) == 0){
             }
-            set.add(s.charAt(i));
-            result = Math.max(result, i - l + 1);
-        }
-        return result;
+                l++;
+                map.put(s.charAt(l), map.getOrDefault(s.charAt(l), 1) - 1);
+                max = Math.max(i-l, max);
+            while(map.getOrDefault(s.charAt(i), -1) > 1){
+            char leftChar = s.charAt(l);
+            // map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        for(int i = 0; i < s.length(); i++){
+        int l = 0;
+        int max = 0;
+        
+        Map<Character, Integer> map = new HashMap<>();
+    public static int revisionSolve(String s){
+    }
+        return revisionSolve(s);
+        if(s == null || s.length == 0) return 0;
+        if(s == " ") return 1;
