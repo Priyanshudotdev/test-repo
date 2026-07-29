604d86fb685cc63932a5c9027327f3630af28d5a
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-07-29 20:48:11 +0530

    Sync leetcode submission: longest-palindromic-substring

--- problems/longest-palindromic-substring/solution.java
+++ problems/longest-palindromic-substring/solution.java
@@ -1,18 +1,20 @@
-class Solution {
-    public String longestPalindrome(String s) {
-            return reSolve(s);
-    }
-    public static String reSolve(String s){
-        if(s == null || s.length() == 0) return "";
-        String result = "";
-        int maxLen = 0;
-        for (int i = 0; i < s.length(); i++) {
-            // odd string
-            int l = i;
-            int r = i;
-            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
-                int currLen = r - l + 1;
-                // if (currLen > maxLen) {
-                //     maxLen = currLen;
+                l--;
+                }
+                    result = s.substring(l, r + 1);
+                if (currLen > result.length()){
+                    maxLen = currLen;
                 //     result = s.substring(l, r + 1);
                 // }
+                // if (currLen > maxLen){
+                //     maxLen = currLen;
+            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                int currLen = r - l + 1;
+            // even string
+            l = i;
+            r = i + 1;
+            }
+                l--;
+                r++;
+                }
+                    result = s.substring(l, r + 1);
+                    maxLen = currLen;
