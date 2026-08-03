7bb0d008b6e0d48cc59134d5d68909347fb5f8c3
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-08-03 23:13:43 +0530

    Sync leetcode submission: 4sum

---
+++ problems/4sum/solution.java
@@ -0,0 +1,17 @@
+                    int sum = nums[i] + nums[j] + nums[low] + nums[high];
+                    if(sum < target){
+                        low++;
+                    }else if(sum > target){
+                        high--;
+                    }else{
+                        List<Integer> list = new ArrayList<>(List.of(nums[i], nums[j], nums[low], nums[high]));
+                while(low < high){
+                int high = nums.length - 1;
+                int low = j + 1;
+            for(int j = i + 1; j < nums.length; j++){
+        for(int i = 0; i < nums.length; i++){
+            if(i < 0 && nums[i] == nums[i-1]) continue;
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+class Solution {
