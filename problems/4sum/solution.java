38bce2c8f505373dc75eb7f13a6687acf3648ce5
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-08-03 23:18:05 +0530

    Sync leetcode submission: 4sum

--- problems/4sum/solution.java
+++ problems/4sum/solution.java
@@ -1,18 +1,19 @@
                         List<Integer> list = new ArrayList<>(List.of(nums[i], nums[j], nums[low], nums[high]));
                     }else{
                         high--;
-                    }else if(sum > target){
+                    }else if(sum > (long) target){
                         low++;
-                    if(sum < target){
-                    int sum = nums[i] + nums[j] + nums[low] + nums[high];
+                    if(sum < (long) target){
+                    long sum = (long) nums[i] + nums[j] + nums[low] + nums[high];
+                        Collections.sort(list);
+                        if(!result.contains(list)){
+                            result.add(list);
+                        }
+                        low++;
+                        while(low < nums.length && nums[low] == nums[low-1]){
                 while(low < high){
-                int low = j + 1;
                 int high = nums.length - 1;
+                int low = j + 1;
             for(int j = i + 1; j < nums.length; j++){
-        for(int i = 0; i < nums.length; i++){
             if(i < 0 && nums[i] == nums[i-1]) continue;
-        Arrays.sort(nums);
-        
-        List<List<Integer>> result = new ArrayList<>();
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-class Solution {
+        for(int i = 0; i < nums.length; i++){
