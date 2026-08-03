49b65a0fa7f08bc71484d2b41aec873789ad95eb
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-08-03 18:53:33 +0530

    Sync leetcode submission: 3sum

--- problems/3sum/solution.java
+++ problems/3sum/solution.java
@@ -1,14 +1,18 @@
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        if(nums.length == 0) return new ArrayList<>(); 
-        List<List<Integer>> result = new ArrayList<>();
-        
+                }else if(sum < 0){
+                    j++;
+                }else {
+                    result.add(List.of(nums[i],nums[j],nums[k]));
+                    k--;
+                if(sum > 0){
+                int sum = nums[i] + nums[j] + nums[k];
+            while(j < k){
+            int k = nums.length - 1;
+        for(int i = 0; i < nums.length; i++){
+            int j = i + 1;
         Arrays.sort(nums);
-        int i = 0;
-        int j = i + 1;
-        while(j<nums.length){
-            int need = -1 * (nums[i] + nums[j]);
-            for(int k = 0; k < nums.length; k++){
-                if(nums[k] == need && k != i && k != j){
-                    List<Integer> list = new ArrayList<>(List.of(nums[i], nums[j], nums[k]));
-                    Collections.sort(list);
+        List<List<Integer>> result = new ArrayList<>();
+        // sort the array
+        if(nums.length == 0) return new ArrayList<>(); 
+                    j++;
+                    while(j < nums.length && nums[j] == nums[j-1]){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
