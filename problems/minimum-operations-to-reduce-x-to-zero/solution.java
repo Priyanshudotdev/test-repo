b468597c473cbb5bd6246a2d5222d1805138d872
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-08-06 12:04:59 +0530

    Sync leetcode submission: minimum-operations-to-reduce-x-to-zero

--- problems/minimum-operations-to-reduce-x-to-zero/solution.java
+++ problems/minimum-operations-to-reduce-x-to-zero/solution.java
@@ -1,15 +1,17 @@
-            int leftSum = x - nums[left];
-            if(leftSum < rightSum && leftSum >= 0){
-            int rightSum = x - nums[right];
-                currSum += nums[left];
-            }else{
                 left++;
-                currSum += nums[right];
-                right--;                
             }
-            op++;
-       while(left <= right){
-       int currSum = 0;
-       int op = 0;
+            if (sum == rem) longest = Math.max(longest, right - left + 1);
+        }
+    }
+    public int notWorkingMethod(int[] nums, int x){
+        int left = 0;
        int right = nums.length - 1;
-       int left = 0;
+       int op = 0;
+                sum -= nums[left];
+            while (sum > rem) {
+            sum += nums[right];
+        for (int right = 0; right < n; right++) {
+        if(rem == 0) return n;
+        if(rem < 0) return -1;
+        return longest == 0 ? -1 : n - longest;
+       int currSum = x;
