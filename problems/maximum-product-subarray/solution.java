f4934522c8b3ef025c195708d0c5f80b20c203bf
Priyanshu Kayarkar <148684260+Priyanshudotdev@users.noreply.github.com>
2026-08-13 20:34:55 +0530

    Sync leetcode submission: maximum-product-subarray

--- problems/maximum-product-subarray/solution.java
+++ problems/maximum-product-subarray/solution.java
@@ -7,13 +7,11 @@
         int min = nums[0];
         int ans = nums[0];
         for(int i = 1; i < nums.length; i++){
-            mul *= num;
-        for(int num: nums){
-        int ans = Integer.MIN_VALUE;
-        int mul = 1;
-    public int maxProd(int[] nums){
+            int temp = max;
+            max = Math.max(Math.max(nums[i] * max, nums[i] * min), nums[i]);
+            min = Math.max(Math.min(nums[i] * temp, min * nums[i]), nums[i]);
+            ans = Math.max(ans,max);
+        }
+        return ans;
     }
-        
-        return maxProd(nums);
-    public int maxProduct(int[] nums) {
-class Solution {
+}
